2b55ecac0188de89aaf8514bb919c25ba0d271d1
metinsenturk <metinsenturk@me.com>
2018-04-19 22:51:12 -0400

    methodology completion

--- Scripts/glm-codes.R
+++ Scripts/glm-codes.R
@@ -1,13 +1,14 @@
-# Logistic Regression Model
+# Logistic Regression Model ====
 
-install.packages("dplyr")
-
-library(dplyr)
+"
+  Methodology for doing lgm.
+"
 
 # importing dataset
 data_raw = read.csv("./Dataset/Heart.csv")
 head(data_raw)
 
+# Data Examining ====
 # examining variables
 str(data_raw)
 
@@ -23,13 +24,21 @@ data_raw$Slope = as.factor(data_raw$Slope)
 summarise_if(data_raw, is.atomic, funs(sum(is.na(.))))
 filter((data_raw), is.na(Ca) | is.na(Thal))
 
+# for reproducable results, seed. TODO: we need to change this to kfold
+set.seed(1000)
+
+# creating dataset for train and test
+ind = sample(x = 2, size = nrow(data_raw), replace = T, prob = c(0.9, 0.1))
+data_train = data_raw[ind == 1, ]
+data_test = data_raw[ind == 2, ]
+
 # frequency tables of AHD and factorial variables
-tab1 = ftable(xtabs(~ AHD + Sex, data = data_raw)) 
-tab2 = ftable(xtabs(~ AHD + Fbs, data = data_raw))
-tab3 = ftable(xtabs(~ AHD + RestECG, data = data_raw))
-tab4 = ftable(xtabs(~ AHD + ExAng, data = data_raw))
-tab5 = ftable(xtabs(~ AHD + Ca, data = data_raw))
-tab6 = ftable(xtabs(~ AHD + Slope, data = data_raw))
+tab1 = ftable(xtabs(~ AHD + Sex, data = data_train)) 
+tab2 = ftable(xtabs(~ AHD + Fbs, data = data_train))
+tab3 = ftable(xtabs(~ AHD + RestECG, data = data_train))
+tab4 = ftable(xtabs(~ AHD + ExAng, data = data_train))
+tab5 = ftable(xtabs(~ AHD + Ca, data = data_train))
+tab6 = ftable(xtabs(~ AHD + Slope, data = data_train))
 
 # running chisq test to understand correlation btw pairs. The smaller the better in terms of relationship. tab2 found to be high in p.
 lapply(list(tab1,tab2,tab3,tab4,tab5,tab6), chisq.test)
@@ -38,11 +47,35 @@ chisq.test(tab2)
 tab2
 
 # examining factor levels
-sapply(list(data_raw$Sex, data_raw$Fbs, data_raw$RestECG, data_raw$Ca, data_raw$Slope), levels)
+sapply(list(data_train$Sex, data_train$Fbs, data_train$RestECG, data_train$Ca, data_train$Slope), levels)
 
+# Models ====
+# logistic regression model
+lgm_model = glm(AHD ~ ., data = data_train, family = binomial)
+summary(lgm_model)
 
+# train results ====
+# prediction
+p_tr_probs = predict(lgm_model, data_train, type = "response")
+head(p_tr_probs)
+head(data_train)
 
+# misclassification error and confusion matrix
+p_tr_preds <- ifelse(p_tr_probs > 0.5, 1, 0)
+tab_tr_cm <- table(predicted = p_tr_preds, actual = data_train$AHD)
+tab_tr_cm
+mse <- 1 - sum(diag(tab_tr_cm))/sum(tab_tr_cm)
+mse
 
+# test results ====
+# prediction
+p_te_probs = predict(lgm_model, data_test, type = "response")
+head(p_te_probs)
+head(data_train)
 
-
-
+# misclassification error and confusion matrix
+p_te_preds <- ifelse(p_te_probs > 0.5, 1, 0)
+tab_te_cm <- table(predicted = p_te_preds, actual = data_test$AHD)
+tab_te_cm
+mse <- 1 - sum(diag(tab_te_cm))/sum(tab_te_cm)
+mse
